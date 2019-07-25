b1f67f4384d62ef74746e16367d27c9de7da4d59
alex <dinglc@gmail.com>
2019-07-25 11:13:09 +0800

    add files

---
+++ custom/ncip/ncipinitialdata/gensrc/com/ncip/initialdata/constants/GeneratedNcipInitialDataConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 2019/7/25 上午 08:02:51                       ---
+ * ----------------------------------------------------------------
+ */
+package com.ncip.initialdata.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedNcipInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "ncipinitialdata";
+	
+	protected GeneratedNcipInitialDataConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
