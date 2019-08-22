dfccbbe641bcaa5ef712f200d55632c52ebe882b
joneslian <joneslian@systex.com>
2019-08-22 12:07:44 +0800

    add-b2bapprovalprocess-extension v1

--- custom/b2bapprovalprocess/src/de/hybris/platform/b2b/jalo/B2BApprovalprocessManager.java
+++ custom/b2bapprovalprocess/src/de/hybris/platform/b2b/jalo/B2BApprovalprocessManager.java
@@ -1,7 +1,8 @@
 /*
- *  
  * [y] hybris Platform
- * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ *
+ * Copyright (c) 2018 SAP SE or an SAP affiliate company.  All rights reserved.
+ *
  * This software is the confidential and proprietary information of SAP
  * ("Confidential Information"). You shall not disclose such Confidential
  * Information and shall use it only in accordance with the terms of the
@@ -9,21 +10,21 @@
  */
 package de.hybris.platform.b2b.jalo;
 
-import de.hybris.platform.b2b.constants.B2BApprovalprocessConstants;
+import de.hybris.platform.b2b.constants.B2BApprovalConstants;
 import de.hybris.platform.jalo.JaloSession;
 import de.hybris.platform.jalo.extension.ExtensionManager;
+
 import org.apache.log4j.Logger;
 
-@SuppressWarnings("PMD")
+
 public class B2BApprovalprocessManager extends GeneratedB2BApprovalprocessManager
 {
 	@SuppressWarnings("unused")
-	private static final Logger log = Logger.getLogger( B2BApprovalprocessManager.class.getName() );
-	
+	private static Logger LOG = Logger.getLogger(B2BApprovalprocessManager.class.getName());
+
 	public static final B2BApprovalprocessManager getInstance()
 	{
-		ExtensionManager em = JaloSession.getCurrentSession().getExtensionManager();
-		return (B2BApprovalprocessManager) em.getExtension(B2BApprovalprocessConstants.EXTENSIONNAME);
+		final ExtensionManager em = JaloSession.getCurrentSession().getExtensionManager();
+		return (B2BApprovalprocessManager) em.getExtension(B2BApprovalConstants.EXTENSIONNAME);
 	}
-	
 }
--- custom/b2bapprovalprocess/src/de/hybris/platform/b2b/jalo/B2BPermission.java
+++ custom/b2bapprovalprocess/src/de/hybris/platform/b2b/jalo/B2BPermission.java
@@ -1,7 +1,8 @@
 /*
- *  
  * [y] hybris Platform
- * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ *
+ * Copyright (c) 2018 SAP SE or an SAP affiliate company.  All rights reserved.
+ *
  * This software is the confidential and proprietary information of SAP
  * ("Confidential Information"). You shall not disclose such Confidential
  * Information and shall use it only in accordance with the terms of the
@@ -13,22 +14,27 @@ import de.hybris.platform.jalo.Item;
 import de.hybris.platform.jalo.JaloBusinessException;
 import de.hybris.platform.jalo.SessionContext;
 import de.hybris.platform.jalo.type.ComposedType;
-import org.apache.log4j.Logger;
 
-public abstract class B2BPermission extends GeneratedB2BPermission
+
+public class B2BPermission extends GeneratedB2BPermission
 {
-	@SuppressWarnings("unused")
-	private static final Logger LOG = Logger.getLogger( B2BPermission.class.getName() );
-	
+
+	public B2BPermission()
+	{
+		// empty
+	}
+
+
 	@Override
-	protected Item createItem(final SessionContext ctx, final ComposedType type, final ItemAttributeMap allAttributes) throws JaloBusinessException
+	protected Item createItem(final SessionContext ctx, final ComposedType type, final ItemAttributeMap allAttributes)
+			throws JaloBusinessException
 	{
 		// business code placed here will be executed before the item is created
 		// then create the item
-		final Item item = super.createItem( ctx, type, allAttributes );
+		final Item item = super.createItem(ctx, type, allAttributes);
 		// business code placed here will be executed after the item was created
 		// and return the item
 		return item;
 	}
-	
+
 }
